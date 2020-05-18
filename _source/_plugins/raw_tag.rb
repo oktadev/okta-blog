11fd7000e30c3ae8dc6b6acc9550863bb738af0e
Brian Demers <bdemers@apache.org>
2020-05-18 13:04:31 -0400

    Adds post: travis-ci-to-github-actions
    Also adds a simple "raw_tag" plugin, the GH Actions expression syntax conflicts with latex syntax

---
+++ _source/_plugins/raw_tag.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class RawTag < Liquid::Block
+    def parse(tokens)
+      @nodelist ||= []
+      @nodelist.clear
+
+      while token = tokens.shift
+        if token =~ FullToken
+          if block_delimiter == $1
+            end_tag
+            return
+          end
+        end
+        @nodelist << token if not token.empty?
+      end
+    end
+  end
+end
