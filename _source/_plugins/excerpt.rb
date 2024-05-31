e4b3986f3ccd0b56f0bb78de8f431566082d4afb
Alisa <7586237+alisaduncan@users.noreply.github.com>
2024-05-31 15:55:01 -0700

    Add post excerpt Liquid tag

---
+++ _source/_plugins/excerpt.rb
@@ -0,0 +1,36 @@
+module Jekyll
+  # A simple tag for YouTube include using an iframe
+    class ExcerptTag < Liquid::Tag
+      def initialize(_tag_name, url, _tokens)
+        @article_id = url.strip
+      end
+
+      def render(context)
+        posts = context.registers[:site].posts
+        post = posts.docs.find {|doc| doc.id == @article_id}
+
+        if !post.nil?
+          title = post['title']
+          # one day figure out how to add author
+          # author_id = post['author']
+          # author = context.registers[:site].data['authors'][author_id]
+          # avatar = author['avatar']
+          # display_name = author['full_name']
+          description = post['description']
+
+          %(<article class="link-container" style="border: 1px solid silver; border-radius: 3px; padding: 12px 15px">
+              <a href='#{@article_id}' style="font-size: 1.375em; margin-bottom: 20px;">
+                <span>#{title}</span>
+              </a>
+              <p>#{description}</p>
+          </article>)
+        else
+          %(<div class="link-container">
+              <a href='#{@article_id}'>Article not found!</a>
+          </div>)
+        end
+      end
+    end
+  end
+
+  Liquid::Template.register_tag('excerpt', Jekyll::ExcerptTag)
